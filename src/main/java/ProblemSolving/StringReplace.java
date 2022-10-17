768b4d659288cec1febc76a693b657b20fe0ee16
fahmy <mohammedfahmi606@gmail.com>
2022-10-17 12:49:36 +0200

    first commit

---
+++ src/main/java/ProblemSolving/StringReplace.java
@@ -0,0 +1,30 @@
+package ProblemSolving;
+
+public class StringReplace {
+    /**
+     * Requirements:
+     *
+     *  0- you should interrogate the interviewer for questions about
+     *    constrains and boundary conditions
+     *
+     *  a working example
+     *      formatString(
+     *          "My name is Milan, people know me as Milan Vasic",
+     *          "Milan",
+     *          "Milan Vasic"
+     *      );
+     *  returns:
+     *      "My name is Milan Vasic, people know me as Milan Vasic"
+     *
+     *  1- your solution should be in O(n), where n is the number of characters in the input string
+     *  2- use Test Driven Development when solving this problem.
+     *  3- use SOLID principles when you can
+     *  4- don't use Java's String.replaceAll or String.replace
+     *
+     *
+     * */
+    public String stringReplace(String input, String toBeReplaced, String replacement) {
+        String result = "";
+        return result;
+    }
+}
